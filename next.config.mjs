b775eeb24f5631e0b0ce96a2e9ce7c46295d90f8
Anubhab Das <anubhabdas111@gmail.com>
2025-11-01 07:24:17 +0000

    Waiting for your web server to start serving [?25h
    > nextn@0.1.0 dev
    >

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['three', '@react-three/drei', 'three-stdlib'],
-  experimental: {
-    serverComponentsExternalPackages: ['@react-three/drei'],
-  },
 };
 
 export default nextConfig;
