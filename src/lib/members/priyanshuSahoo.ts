d6b902be07b5cc716dfd9bc3c943ac55ddcaa2da
anubhab7111 <anubhabdas111@gmail.com>
2025-11-02 13:56:55 +0530

    commit

--- src/lib/members/priyanshuSahoo.ts
+++ src/lib/members/priyanshuSahoo.ts
@@ -10,7 +10,7 @@ export const priyanshuSahoo: Member = {
   skills: [
     { name: 'VR Development', level: 87 },
     { name: 'Game Mechanics', level: 84 },
-    { name: 'C# / Unity', level: 82 },
+    { name: 'Unity', level: 82 },
   ],
   specialAbility: 'Striker',
   bio: 'Develops immersive game mechanics and contributes to VR development. Focused on enhancing gameplay experiences and collaborating with teammates to bring creative visions to life.',
